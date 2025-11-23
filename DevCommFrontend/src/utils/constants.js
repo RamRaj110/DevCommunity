f2e93f17738c2072465656d23a31bb2decaaad00
RamRaj110 <rampawankumar110@gmail.com>
2025-11-23 23:27:03 +0530

    /api/

--- DevCommFrontend/src/utils/constants.js
+++ DevCommFrontend/src/utils/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:3000/";//for local
+// export const BASE_URL = "http://localhost:3000/";//for local
 // export const BASE_URL = 'https://super-space-umbrella-9455pg79x9xhxgj6-3000.app.github.dev' //for online
 
 
-// export const BASE_URL = "/api/" 
+export const BASE_URL = "/api/" 
 
 // ssh -i "DevCommunity.pem" ubuntu@ec2-3-106-130-58.ap-southeast-2.compute.amazonaws.com
